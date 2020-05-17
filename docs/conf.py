874e5e9cf7b5128cd925c5405893c297de04ad09
Kkevsterrr <user@example.com>
2020-05-17 10:35:52 -0400

    Changed master doc

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,8 @@ extensions = [
     "sphinx.ext.autosectionlabel"
 ]
 
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
